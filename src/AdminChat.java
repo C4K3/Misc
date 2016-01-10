9a07f2875d34246d717d48b3334495cb96ab8e05
Sebastian Blunt <sebastian@c4k3.net>
2016-01-10 20:50:22 +0100

    Cleanup AdminChat

--- src/AdminChat.java
+++ src/AdminChat.java
@@ -8,51 +8,41 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class AdminChat implements CommandExecutor {
-	/* Adds the /a command that lets OPs talk in private
+	/*
+	 * Adds the /a command that lets OPs talk in private
 	 * Only OPs can use the command
 	 * The commands arguments are displayed to all other OPs on the server
 	 */
 
 	String splayer;
-
 	String sargs;
 
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		if (cmd.getName().equals("a")) {
 
-		if ( cmd.getName().equals("a")){
-
-			if  ( sender.isOp() ) {
-
-				splayer = sender.getName();
-
-				sargs = "";
-
-				for ( int i = 0; i < args.length; i++ ) {
-					sargs = sargs + " " + args[i];
-				}
-
-				Bukkit.getLogger().info("[" + splayer + "]" + sargs );
-
-				/* For all online players, if player isOp sendMessage */
-				for (Player player : Bukkit.getOnlinePlayers()) {
-
-					if ( player.isOp()) {
-
-						player.sendMessage("<" + ChatColor.GOLD + splayer + ChatColor.RESET + ">" + sargs );
-
-					}
-
-				}
-
-			} else {
-				/* If player is not OP */
+			if  (!sender.isOp()) {
 				sender.sendMessage(ChatColor.RED + "You do not have permission to use this command");
 				return false;
 			}
 
+			splayer = sender.getName();
+
+			sargs = "";
+			for (int i = 0; i < args.length; i++)
+				sargs += " " + args[i];
+
+			Misc.instance.getLogger().info("[" + splayer + "]" + sargs);
+
+			/* For all online players, if player isOp sendMessage */
+			for (Player player : Misc.instance.getServer().getOnlinePlayers()) {
+				if (player.isOp())
+					player.sendMessage("<" + ChatColor.GOLD + splayer
+							+ ChatColor.RESET + ">" + sargs);
+			}
+
 		}
 
-		return false;
+		return true;
 	}
 
 }
