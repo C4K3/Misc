c3bd4678be916a2cbddaf361415ce6849f343595
Sebastian Blunt <sebastian@c4k3.net>
2016-01-10 23:13:39 +0100

    Rewrite AdminTeleport

--- src/AdminTeleport.java
+++ src/AdminTeleport.java
@@ -9,373 +9,233 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+/**
+ * Adds the /tp, /tpc, /tpw and /tpo commands
+ */
 public class AdminTeleport implements CommandExecutor {
-	/* Adds the /tp, /tpc and /tps commands */
 
-	private String scmd;
-
-	private int argslength;
-
-	private Player tplayer;
-
-	private Location loc = new Location(null, 0, 0, 0);
-
-	private World world;
-
-	private String splayer; //String of senders name
-
-	private String stplayer; //String of targets name (if target is a player)
-
-	/** Gets all the teleport commands
-	 * 
-	 * Returns true if somebody was teleported. Else returns false
-	 */
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-
-		scmd = cmd.getName();
-
 		Player player = null;
-		if (sender instanceof Player){
+		if (sender instanceof Player)
 			player = (Player) sender;
+
+		if (player != null && !player.isOp()) {
+			player.sendMessage(ChatColor.RED
+					+ "You do not have permission to use this command.");
+			return true;
 		}
 
-		splayer = sender.getName();
+		String scmd = cmd.getName();
 
-		/* /tp command
-		 * Teleports sender to target player
-		 * Autofills names
-		 * Syntax: /tp <target player>
-		 */
-		if ( scmd.equals("tp") && player != null ) {
+		if (scmd.equals("tp")) {
+			tp(player, args);
+		} else if (scmd.equals("tpc")) {
+			tpc(player, args);
+		} else if (scmd.equals("tpo")) {
+			tpo(player, args);
+		} else if (scmd.equals("tpw")) {
+			tpw(player, args);
+		}
 
-			if ( sender.isOp() ) {
-				/* Sender is OP */
+		return true;
 
-				argslength = args.length;
+	}
 
-				if ( argslength == 1 ) {
-					/* Args are of proper length */
+	/**
+	 * Teleports sender to target player
+	 * Autofills names
+	 */
+	private void tp(Player player, String[] args) {
+		if (player == null) {
+			Misc.instance.getLogger().info("You cannot use this command.");
+			return;
+		}
 
-					tplayer = Bukkit.getPlayer(args[0]);
+		if (args.length != 1) {
+			player.sendMessage(ChatColor.RED
+					+ "Invalid usage.\n"
+					+ "Correct usage is: /tp <player>");
+			return;
+		}
 
-					if ( tplayer == null ) {
-						/* Target player is null (player was not found) */
-						sender.sendMessage(ChatColor.RED + "Target player not found");
-						return false;
+		@SuppressWarnings("deprecation") /* Only used for currently online players */
+		Player target = Misc.instance.getServer().getPlayer(args[0]);
 
-					} else {
-						/* Target player was found (teleporting sender to target) */
+		if (target == null) {
+			player.sendMessage(ChatColor.RED
+					+ "The specified player was not found.");
+			return ;
+		}
 
-						stplayer = tplayer.getName();
+		if (player.isInsideVehicle())
+			player.leaveVehicle();
+		player.teleport(target);
 
-						/* Notifying all OPs on the server of this */
-						for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+		notify_admins(player, "Teleported " + player.getName() + " to " + target.getName());
+	}
 
-							if ( onlinePlayer.isOp()) {
+	/**
+	 * Teleports the sender (or other player if specified) to the given coords
+	 */
+	private void tpc(Player player, String[] args) {
+		if (args.length > 5 || args.length < 3) {
+			tpc_usage(player, "Invalid amount of arguments.");
+			return;
+		}
 
-								onlinePlayer.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + "[" + splayer + ": Teleported " + splayer + " to " + stplayer + "]");
+		if (args.length < 5 && player == null) {
+			tpc_usage(player, "If you want to use this command as a non-player,"
+					+ " you must specify a player.");
+			return;
+		}
 
-							}
+		// FIXME: See if the bogus coords are necessary to initialize
+		Location loc = new Location(Misc.instance.getServer().getWorld("world"), 0, 0, 0);
 
-						}
+		try {
+			loc.setX(Integer.parseInt(args[0]) + 0.5);
+			loc.setY(Integer.parseInt(args[1]) + 0.5);
+			loc.setZ(Integer.parseInt(args[2]) + 0.5);
+		} catch (Exception e) {
+			tpc_usage(player, "Unable to parse coordinates to numbers.");
+			return;
+		}
 
-						if ( player.isInsideVehicle() ) player.leaveVehicle();
-						player.teleport(tplayer);
-
-						return true;
-
-					}
-
-				}
-
-				if ( argslength == 0 ) {
-					/* Arguments length is 0 (sender didn't specify any target) */
-					sender.sendMessage(ChatColor.RED + "You need to specify a target");
-					return false;
-				}
-
-				if ( argslength > 1 ) {
-					sender.sendMessage(ChatColor.RED + "You cannot specify more than one target");
-					return false;
-				}
-
-			} else {
-				/* Sender is not OP */
-				sender.sendMessage(ChatColor.RED + "You do not have permission to use this command\n" +
-						"With the exception of /world and /kill, teleportation is disabled on this server");
-				return false;
+		if (args.length > 3) {
+			World world = Misc.instance.getServer().getWorld(args[3]);
+			if (world == null) {
+				tpc_usage(player, "No such world found. Note that some worlds"
+						+ " are prepended with world_, e.g. world_the_nether");
+				return;
 			}
 
+			loc.setWorld(Misc.instance.getServer().getWorld(args[3]));
+
+		} else if (player != null) {
+			loc.setWorld(player.getWorld());
+		} else {
+			tpc_usage(player, "If you want to use this command as a non-player,"
+					+ " you have to specify a world.");
+			return;
 		}
 
-		/* /tpo command
-		 * Teleports target to sender
-		 * Autofills names
-		 * Syntax: /tps <target player>
-		 */
-		if ( scmd.equals("tpo") && player != null ) {
+		Player target;
+		if (args.length > 4) {
+			/* do it this way because for some reason you can't use annotations
+			 * on simple assignments */
+			@SuppressWarnings("deprecation") /* Only used for currently online players */
+			Player tmp = Misc.instance.getServer().getPlayer(args[4]);
+			target = tmp;
 
-			if ( sender.isOp() ) {
-				/* Sender is OP */
-
-				argslength = args.length;
-
-				if ( argslength == 1 ) {
-					/* Correct amount of arguments */
-
-					tplayer = Bukkit.getPlayer(args[0]);
-
-					if ( tplayer == null ) {
-						/* Target player is null (player was not found) */
-						sender.sendMessage(ChatColor.RED + "Target player not found");
-						return false;
-
-					} else {
-						/* Target player was found (teleporting target to sender) */
-
-						stplayer = tplayer.getName();
-
-						/* Notifying all OPs on the server of this */
-						for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-
-							if ( onlinePlayer.isOp()) {
-
-								onlinePlayer.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + "[" + splayer + ": Teleported " + stplayer + " to " + splayer + "]");
-
-							}
-
-						}
-
-						if ( tplayer.isInsideVehicle() ) tplayer.leaveVehicle();
-						tplayer.teleport(player);
-
-						return true;
-
-					}
-
-				}
-
-				if ( argslength == 0 ) {
-					/* Not enough arguments (sender didn't specify a target) */
-					sender.sendMessage(ChatColor.RED + "You need to specify a target");
-					return false;
-				}
-
-				if ( argslength > 1 ) {
-					/* Too many arguments */
-					sender.sendMessage(ChatColor.RED + "You cannot specify more than one target");
-					return false;
-				}
-
-			} else {
-				/* Sender is not OP */
-				sender.sendMessage(ChatColor.RED + "You do not have permission to use this command\n" +
-						"With the exception of /world and /kill, teleportation is disabled on this server");
-				return false;
+			if (target == null) {
+				tpc_usage(player, "The specified player was not found.");
+				return;
 			}
-
+		} else {
+			target = player;
 		}
 
-		/* /tpc command
-		 * Teleports sender to target coordinates
-		 * If a 4th argument is supplied, it is taken as the target world
-		 * Syntax: /tpc <x> <y> <z> [world]
-		 * */
-		if ( scmd.equals("tpc") ) {
+		if (target.isInsideVehicle())
+			target.leaveVehicle();
+		target.teleport(loc);
 
-			if ( sender.isOp() ) {
+		notify_admins(player, "Teleported " + target.getName() + " to coordinates");
+	}
 
-				argslength = args.length;
+	/**
+	 * Displays usage info for tpc
+	 */
+	private void tpc_usage(Player player, String error) {
+		error += "\n"
+			+ "Proper syntax is /tpc <x> <y> <z> [world] [player]";
 
-				if ( argslength == 3 || argslength == 4 || argslength == 5 ) {
-					/* Correct amount of arguments */
+		if (player == null) {
+			Misc.instance.getLogger().info(error);
+		} else {
+			player.sendMessage(ChatColor.RED + error);
+		}
+	}
 
-					/* Checking if sender is a player or not. Non-players should always enter the full command (argslength should equal 5) */
-					if ( player == null && argslength != 5 ) {
-						Bukkit.getLogger().info("If you want to use this command as a non-player, you must use the full command: /tpc <x> <y> <z> [world] [player]");
-						return false;
-					}
-
-					/* Try to convert arguments into doubles for target location */
-					try {
-
-						loc.setX(Integer.parseInt(args[0]));
-						loc.setY(Integer.parseInt(args[1]));
-						loc.setZ(Integer.parseInt(args[2]));
-
-					} catch (Exception e) {
-						/* Exception is most likely caused by parseInt not being able to convert string to an integer
-						 * (== sender entered a string, not coordinates in the first 3 arguments)
-						 */
-						sender.sendMessage(ChatColor.RED +  "Unable to convert input arguments into location\n" +
-								"Proper syntax is /tpc <x> <y> <z> [world] [player]");
-						Bukkit.getLogger().info("Expected parseInt exception: " + e);
-						return false;
-					}
-
-					/* If there are 5 args, then the sender is trying to teleport an other person. Trying to get said person*/
-					if ( argslength == 5 ) {
-
-						tplayer = Bukkit.getPlayer(args[4]);
-
-						if ( tplayer == null ) {
-
-							/* Target player is null (player was not found) */
-							sender.sendMessage("Target player not found");
-							return false;
-
-						}
-
-						stplayer = tplayer.getName();
-
-					} else {
-						/* Argslength is less than 5, therefore player is not trying to teleport a different person, meaning we can set tplayer to the sender */
-						tplayer = player;
-						stplayer = splayer;
-					}
-
-					if ( tplayer.isInsideVehicle() ) tplayer.leaveVehicle();
-
-					if ( argslength == 4 || argslength == 5 ) {
-						/* The sender is trying to teleport to a specific world */
-
-						try {
-							/* Try to get world from fourth argument */							
-							loc.setWorld(Bukkit.getWorld(args[3]));
-
-							/* Notifying all OPs on the server of this */
-							for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-
-								if ( onlinePlayer.isOp()) {
-
-									onlinePlayer.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + "[" + splayer + ": Teleported " + stplayer + " to coordinates]");
-
-								}
-
-							}
-
-							tplayer.teleport(loc);
-
-						} catch (Exception e) {
-							/* Exception is most likely caused by Bukkit.getWorld being unable to get the world
-							 * (== world does not exist)
-							 */
-							sender.sendMessage(ChatColor.RED + "Unable to get world '" + args[3] + "'" +
-									"\nPlease note some worlds use the world_ prefix\n(eg world_nether, world_the_end)");
-							Bukkit.getLogger().info("Expected Bukkit.getWorld exception: " + e);
-							return false;
-
-						}
-
-					} else {
-						/* Argslength is 3
-						 * (== sender did not specify a world)
-						 * therefore we use the world the sender is currently in
-						 */						
-						loc.setWorld(player.getWorld());
-
-						/* Notifying all OPs on the server of this */
-						for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-
-							if ( onlinePlayer.isOp()) {
-
-								onlinePlayer.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + "[" + splayer + ": Teleported " + splayer + " to coordinates]");
-
-							}
-
-						}
-
-						if ( player.isInsideVehicle() ) player.leaveVehicle();
-						player.teleport(loc);
-
-					}
-
-				} else {
-					/* Incorrect amount of arguments */
-					sender.sendMessage(ChatColor.RED + "Incorrect amount of arguments.\n" +
-							"Proper syntax is /tpc <x> <y> <z> [world]");
-					return false;
-				}
-
-			} else {
-				/* Sender is not OP */
-				sender.sendMessage(ChatColor.RED + "You do not have permission to use this command\n" +
-						"With the exception of /world and /kill, teleportation is disabled on this server");
-				return false;
-			}
-
-
+	/**
+	 * Teleports the given player to the command sender
+	 */
+	private void tpo(Player player, String[] args) {
+		if (player == null) {
+			Misc.instance.getLogger().info("You cannot use this command.");
+			return;
 		}
 
-		/* /tpw command
-		 * Teleports command sender to spawn of target world
-		 * Syntax: /tpw <world>
-		 */
-		if ( scmd.equals("tpw")) {
-
-			if ( sender.isOp() ) {
-
-				/* If only one argument was supplied.
-				 * Proper syntax is /tpw <world>
-				 * so argument length of 1 is correct
-				 */
-				if ( args.length == 1 ) {
-
-					try {
-						/* Try to get world from argument
-						 * Will error if world is not found
-						 */
-						world = Bukkit.getWorld(args[0]);
-
-						loc = world.getSpawnLocation();
-
-						/* Notifying all OPs on the server of this */
-						for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-
-							if ( onlinePlayer.isOp()) {
-
-								onlinePlayer.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC + "[" + splayer + ": Teleported " + splayer + " to spawn in '" + args[0] + "']");
-
-							}
-
-						}
-
-						if ( player.isInsideVehicle() ) player.leaveVehicle();
-						player.teleport(loc);
-
-						return true;
-
-					} catch (Exception e) {
-						/* Exception is most likely caused by Bukkit.getWorld being unable to get the world
-						 * (== world does not exist)
-						 */
-						sender.sendMessage(ChatColor.RED + "Unable to get world " + args[0] +
-								"\nPlease note some worlds use the world_ prefix\n(eg world_nether, world_the_end)");
-						Bukkit.getLogger().info("Expect Bukkit.getWorld exception: " + e);
-						return false;
-					}
-
-				} else {
-					/* Incorrect amount of arguments (suppose to be 1) */
-					sender.sendMessage(ChatColor.RED + "Incorrect amount of arguments.\n"
-							+ "Proper syntax is /tpw <world>");
-					return false;
-				}
-
-
-			} else {
-				/* Sender is not OP */
-				sender.sendMessage(ChatColor.RED + "You do not have permission to use this command\n" +
-						"With the exception of /world and /kill, teleportation is disabled on this server");
-				return false;
-			}
-
+		if (args.length != 1) {
+			player.sendMessage(ChatColor.RED + "Invalid amount of arguments.\n"
+					+ "Proper usage is: /tpo <player>");
+			return;
 		}
 
-		/* None of the commands could be triggered, likely caused by the sender being console and sending a command other than /tpc */
-		if ( !scmd.equals("tpc") ) Bukkit.getLogger().info("You have to be a player to use this command");
-		return false;
+		@SuppressWarnings("deprecation") /* Only used for online players */
+		Player target = Misc.instance.getServer().getPlayer(args[0]);
 
+		if (target == null) {
+			player.sendMessage(ChatColor.RED + "The specified player was not found.");
+			return;
+		}
+
+		if (target.isInsideVehicle())
+			target.leaveVehicle();
+		target.teleport(player.getLocation());
+
+		notify_admins(player, "Teleported " + target.getName() + " to " + player.getName());
+	}
+
+	/**
+	 * Teleports the sender to the spawn point of the given world
+	 */
+	private void tpw(Player player, String[] args) {
+		if (player == null) {
+			Misc.instance.getLogger().info("You cannot use this command.");
+			return;
+		}
+
+		if (args.length != 1) {
+			player.sendMessage(ChatColor.RED + "Invalid amount of arguments.\n"
+					+ "Proper usage is: /tpw <world>");
+			return;
+		}
+
+		World world = Misc.instance.getServer().getWorld(args[0]);
+
+		if (world == null) {
+			player.sendMessage(ChatColor.RED + "No such world found. Note that some worlds"
+					+ " are prepended with world_, e.g. world_the_nether");
+			return;
+		}
+
+		if (player.isInsideVehicle())
+			player.leaveVehicle();
+		player.teleport(world.getSpawnLocation());
+
+		notify_admins(player, "Teleported " + player.getName()
+				+ " to spawn in '" + world.getName() + "'");
+	}
+
+	/**
+	 * Notifies all admins on the server of the action taken by sender.
+	 */
+	private void notify_admins(Player sender, String message) {
+		String ssender;
+		if (sender == null) {
+			ssender = "CONSOLE";
+		} else {
+			ssender = sender.getName();
+		}
+
+		for (Player p : Misc.instance.getServer().getOnlinePlayers()) {
+			if (p.isOp())
+				p.sendMessage(ChatColor.GRAY + "" + ChatColor.ITALIC
+						+ "[" + ssender + ": " + message + "]");
+		}
+
+		/* Intentional use of bukkit logger */
+		Bukkit.getLogger().info("[" + ssender + ": " + message + "]");
 	}
 
 }
