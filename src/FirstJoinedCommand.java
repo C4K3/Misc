4664704f2855c4152ced63d82b27ac211da5b370
Sebastian Blunt <sebastian@c4k3.net>
2016-05-01 11:53:48 +0200

    Allow people to use /firstjoined on offline players

--- src/FirstJoinedCommand.java
+++ src/FirstJoinedCommand.java
@@ -2,12 +2,22 @@ package net.simpvp.Misc;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.Bukkit;
 
 /**
  * A command to let /everybody/ see when somebody first joined.
@@ -33,25 +43,89 @@ public class FirstJoinedCommand implements CommandExecutor {
 			target = player;
 		}
 
-		if (target == null) {
-			if (player == null) {
-				Misc.instance.getLogger().info("Could not find the specified player.");
-			} else {
-				player.sendMessage(ChatColor.RED + "Could not find the specified player.");
-			}
-
-			return true;
+		final UUID uuid;
+		if (player == null) {
+			uuid = null;
+		} else {
+			uuid = player.getUniqueId();
 		}
 
-		long days = ((System.currentTimeMillis() - target.getFirstPlayed()) / (1000L * 60 * 60 * 24));
-		SimpleDateFormat sdf = new SimpleDateFormat("d MMMM yyyy");
-		String msg = target.getName() + " first joined on "
-			+ ChatColor.AQUA + sdf.format(new Date(target.getFirstPlayed()))
-			+ ChatColor.GREEN + ", " + days + " day";
-		if (days != 1)
-			msg += "s";
-		msg += " ago. Note that for people who joined before 2012,"
-		       + " this command may show a later date than the true date.";
+		if (target == null) {
+			get_uuid(args[0], uuid);
+
+		} else {
+			send_result(uuid, target.getUniqueId(), target.getName());
+		}
+
+		return true;
+	}
+
+	private void get_uuid(final String name, final UUID caller_uuid) {
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				try {
+					URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + name);
+					URLConnection conn = url.openConnection();
+					conn.connect();
+					BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+					String resp = reader.readLine();
+					if (resp == null)
+						resp = "";
+
+					Pattern p = Pattern.compile("\"id\":\"(\\S+?)\"");
+					Matcher m = p.matcher(resp);
+					m.find();
+					String uuid_str = m.group(1);
+					uuid_str = uuid_str.replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
+
+					final UUID uuid = UUID.fromString(uuid_str);
+					new BukkitRunnable() {
+						@Override
+						public void run() {
+							send_result(caller_uuid, uuid, name);
+						}
+					}.runTask(Misc.instance);
+
+				} catch (Exception e) {
+					new BukkitRunnable() {
+						@Override
+						public void run() {
+							send_result(caller_uuid, null, name);
+						}
+					}.runTask(Misc.instance);
+				}
+
+
+			}
+		}.runTaskAsynchronously(Misc.instance);
+	}
+
+	private void send_result(UUID uuid, UUID target_uuid, String target_name) {
+		String msg;
+		if (target_uuid == null) {
+			msg = "Error retrieving data of " + target_name + ".";
+		} else {
+			OfflinePlayer off_player = Misc.instance.getServer().getOfflinePlayer(target_uuid);
+			if (off_player.getFirstPlayed() == 0) {
+				msg = target_name + " has never played on this server.";
+			} else {
+
+				long days = ((System.currentTimeMillis() - off_player.getFirstPlayed())
+						/ (1000L * 60 * 60 * 24));
+				SimpleDateFormat sdf = new SimpleDateFormat("d MMMM yyyy");
+
+				msg = off_player.getName() + " first joined on "
+					+ ChatColor.AQUA + sdf.format(new Date(off_player.getFirstPlayed()))
+					+ ChatColor.GREEN + ", " + days + " day";
+				if (days != 1)
+					msg += "s";
+				msg += " ago. Note that for people who joined before 2012,"
+					+ " this command may show a later date than the true date.";
+			}
+		}
+
+		Player player = Misc.instance.getServer().getPlayer(uuid);
 
 		if (player == null) {
 			Misc.instance.getLogger().info(msg);
@@ -59,7 +133,6 @@ public class FirstJoinedCommand implements CommandExecutor {
 			player.sendMessage(ChatColor.GREEN + msg);
 		}
 
-		return true;
 	}
 
 }
