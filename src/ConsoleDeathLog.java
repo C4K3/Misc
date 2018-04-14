36b7ec91df5ea249de5032cd51906e50653b52fd
Sebastian Blunt <sebastian@c4k3.net>
2018-04-14 14:25:23 +0200

    Log player level and inventory contents on death

--- src/ConsoleDeathLog.java
+++ src/ConsoleDeathLog.java
@@ -5,7 +5,7 @@ import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Logs death messages to console
@@ -20,14 +20,31 @@ public class ConsoleDeathLog implements Listener {
 	public void onPlayerDeath(PlayerDeathEvent event){
 		Location loc = event.getEntity().getLocation();
 
-		/* Intentional use of bukkit logger */
-		Bukkit.getLogger().info(event.getDeathMessage() + " at "
+		String death_msg = event.getDeathMessage() + " at "
 				+ loc.getWorld().getName()
 				+ " " + loc.getBlockX()
 				+ " " + loc.getBlockY()
 				+ " " + loc.getBlockZ()
-				+ " for " + event.getEntity().getName());
-	}
+				+ " for " + event.getEntity().getName();
+		/* Log player level and inventory contents just in case.
+		 * The output log format should be consistent with the
+		 * /eventrestore command format */
+		String sInventoryContents = "";
 
+		for (ItemStack itemStack : event.getEntity().getInventory().getContents()) {
+			if (itemStack == null)
+				continue;
+
+			String tmp = itemStack.getType().toString()
+				+ "." + itemStack.getAmount()
+				+ "." + itemStack.getDurability()
+				+ "." + itemStack.getEnchantments().toString();
+			tmp = tmp.replaceAll(" ", "");
+			sInventoryContents += " " + tmp;
+		}
+
+		/* Intentional use of bukkit logger */
+		Bukkit.getLogger().info(death_msg + ": " + event.getEntity().getLevel() + sInventoryContents);
+	}
 }
 
