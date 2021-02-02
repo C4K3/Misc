7c14964d671c8cb72edb1629a8396763deb8af94
Sebastian Blunt <sebastian@c4k3.net>
2021-02-01 16:55:04 -0800

    Make the arrow owner fix less disruptive

--- src/main/java/net/simpvp/Misc/ArrowOwner.java
+++ src/main/java/net/simpvp/Misc/ArrowOwner.java
@@ -5,6 +5,9 @@ import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import net.minecraft.server.v1_16_R3.Entity;
 import net.minecraft.server.v1_16_R3.NBTTagCompound;
@@ -17,21 +20,33 @@ import org.bukkit.craftbukkit.v1_16_R3.entity.CraftEntity;
  * information, even long after the fact.
  */
 public class ArrowOwner implements Listener {
+	@EventHandler
+	public void onProjectileHit(ProjectileHitEvent event) {
+		final Projectile p = event.getEntity();
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				remove_owner_info(p);
+
+			}
+		}.runTaskLater(Misc.instance, 1L);
+
+	}
 
 	@EventHandler
-	public void onShootBow(EntityShootBowEvent event) {
-		if (!(event.getEntity() instanceof Player)) {
+	public void onChunkLoad(ChunkLoadEvent event) {
+		if (event.isNewChunk()) {
 			return;
 		}
 
-		Projectile p;
-		if (event.getProjectile() instanceof Projectile) {
-			p = (Projectile) event.getProjectile();
-		} else {
-			Misc.instance.getLogger().warning("Shot non-projectile");
-			return;
+		for (org.bukkit.entity.Entity e : event.getChunk().getEntities()) {
+			if (e instanceof Projectile) {
+				remove_owner_info((Projectile) e);
+			}
 		}
+	}
 
+	private void remove_owner_info(Projectile p) {
 		Entity e = ((CraftEntity) p).getHandle();
 		NBTTagCompound nbt = new NBTTagCompound();
 		e.save(nbt);
