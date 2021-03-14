7f6452dac80b3438f535f9f9018afff1a34a8265
Sebastian Blunt <sebastian@c4k3.net>
2021-03-13 22:50:22 -0800

    Thunder: Fix distance computation
    The previous commit was actually wrong, it wasn't abusable, but it was
    computing the location incorrectly.

--- src/main/java/net/simpvp/Misc/Thunder.java
+++ src/main/java/net/simpvp/Misc/Thunder.java
@@ -28,6 +28,7 @@ public class Thunder {
 
 				switch (soundType) {
 					case ENTITY_LIGHTNING_BOLT_THUNDER:
+					// Does this actually work for these two?
 					case EVENT_RAID_HORN:
 					case ENTITY_WITHER_SPAWN:
 						break;
@@ -38,23 +39,36 @@ public class Thunder {
 				/* Change the direction of all thunder packets to
 				 * a random direction, but keep the distance and height
 				 * of the thunder the same, to prevent it from
-				 * sound differently. */
+				 * sound differently.
+				 *
+				 * Note: While the packet does include absolute
+				 * coordinates (multiplied by 8) the vanilla
+				 * server normalizes the packet distance to be
+				 * within range of each player. The server will
+				 * never send packets with absolute coords
+				 * matching that of another player. In other
+				 * words, the distance should never be more
+				 * than a few hundred.
+				 *
+				 * This does have the unfortunate effect of not
+				 * making it viable for us to know if a given
+				 * thunder packet was triggered from a
+				 * lightning strike nearby or from far away.
+				 * Checking that would be desirable, such that
+				 * we could completely skip modifying sounds
+				 * that have a source nearby.*/
 				Location playerlocation = event.getPlayer().getLocation();
-				double origx = p.getIntegers().read(0) - playerlocation.getBlockX();
-				double origz = p.getIntegers().read(2) - playerlocation.getBlockZ();
-				double dist_squared = origx * origx + origz * origz;
-				double dist;
-				if (dist_squared > 1000 * 1000) {
-					dist = 1000.0;
-				} else {
-					dist = Math.sqrt(dist_squared);
-				}
+				double origx = (p.getIntegers().read(0) / 8) - playerlocation.getBlockX();
+				double origz = (p.getIntegers().read(2) / 8) - playerlocation.getBlockZ();
+				double dist = Math.sqrt(origx * origx + origz * origz);
 
 				Random rng = new Random();
 				double angle = Math.toRadians(rng.nextInt(360));
 
-				int new_x = (int) (Math.cos(angle) * dist + playerlocation.getBlockX());
-				int new_z = (int) (Math.sin(angle) * dist + playerlocation.getBlockZ());
+				int new_x = (int) (Math.cos(angle) * dist) + playerlocation.getBlockX();
+				int new_z = (int) (Math.sin(angle) * dist) + playerlocation.getBlockZ();
+				new_x *= 8;
+				new_z *= 8;
 
 				p.getIntegers().write(0, new_x);
 				p.getIntegers().write(2, new_z);
