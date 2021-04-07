b3de2fdf4963e7ce2c8a0f78b447742a4b29aa31
MoWobbler <64741387+MoWobbler@users.noreply.github.com>
2021-04-07 12:07:01 -0400

    Update PotionEffectListener.java

--- src/main/java/net/simpvp/Misc/PotionEffectListener.java
+++ src/main/java/net/simpvp/Misc/PotionEffectListener.java
@@ -8,16 +8,18 @@ import org.bukkit.potion.PotionEffectType;
 //This file disables elder guardians from giving mining fatigue within 100 blocks of spawn
 public class PotionEffectListener implements Listener {
 	
+	List<?> listOfWorlds = Misc.instance.getConfig().getList("disableElderGuardians");
+	
 	@EventHandler
 	public void potionEffectEvent(EntityPotionEffectEvent event) {
 		
 		//Check if world name is in the config
-		if (!Misc.instance.getConfig().getList("disableElderGuardians").contains(event.getEntity().getWorld().getName())) {
+		if (!listOfWorlds.contains(event.getEntity().getWorld().getName())) {
 			return;
 		}
 		
 		//If entity is less than 100 blocks from spawn
-		if (event.getEntity().getLocation().distance(event.getEntity().getWorld().getSpawnLocation()) <= 100) {
+		if (event.getEntity().getLocation().distanceSquared(event.getEntity().getWorld().getSpawnLocation()) <= 100*100) {
 			//If potion equals mining fatigue and the event is from the "attack" cause
 			if (event.getModifiedType().equals(PotionEffectType.SLOW_DIGGING) && event.getCause().equals(EntityPotionEffectEvent.Cause.ATTACK)) {
 				//Cancel event
