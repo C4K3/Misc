98100ac73459050bb20eaf105c94502c8b451f97
Sebastian Blunt <sebastian@c4k3.net>
2021-04-24 14:23:59 -0700

    AdminShortcuts: Add support for ancient_debris

--- src/main/java/net/simpvp/Misc/AdminShortcuts.java
+++ src/main/java/net/simpvp/Misc/AdminShortcuts.java
@@ -4,51 +4,77 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.World.Environment;
 
+import java.util.HashMap;
+import java.util.UUID;
+
+/**
+ * Shortcuts for logblock.
+ */
 public class AdminShortcuts implements CommandExecutor {
 
+	private static HashMap<UUID, String> LAST_X = new HashMap<>();
+
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		Player player = null;
+		if (sender instanceof Player) {
+			player = (Player) sender;
+		};
 
 		String scmd = command.getName();
 
 		String cmd = null;
 
-		/* On /dw runs the command
-		 * /lb destroyed block 56 since 1d sum players world "world" */
 		if (scmd.equalsIgnoreCase("dw")) {
 			cmd = "lb lookup destroyed block diamond_ore since 8d sum players world \"world\"";
-		}
-
-		/* On /dp runs the command
-		 * /lb destroyed block 56 since 1d sum players world "pvp" */
-		if (scmd.equalsIgnoreCase("dp")) {
+		} else if (scmd.equalsIgnoreCase("dp")) {
 			cmd = "lb lookup destroyed block diamond_ore since 8d sum players world \"pvp\"";
-		}
-
-		/* On /x runs the command
-		 * /lb destroyed block 56 since 1d player args[0] coords */
-		if (scmd.equalsIgnoreCase("x")) {
-
-			/* Checking that the correct amount of arguments were entered */
-			if (args.length == 1) {
-				cmd = "lb lookup destroyed block diamond_ore player " + args[0] + " time 0 coords";
-			} else {
-				/* Incorrect amount of arguments */
-				sender.sendMessage(ChatColor.RED + "Incorrect amount of arguments\n" +
-						"Proper syntax is /x <player>");
+		} else if (scmd.equalsIgnoreCase("dn")) {
+			cmd = "lb lookup destroyed block ancient_debris since 8d sum players world \"world_nether\"";
+		} else if (scmd.equalsIgnoreCase("x")) {
+			if (player == null) {
+				sender.sendMessage("You must be a player to use this command.");
 				return true;
 			}
 
-		}
+			UUID uuid = player.getUniqueId();
 
-		if (cmd == null) {
+			String block;
+			String target;
+			if (args.length == 0) {
+				target = LAST_X.get(uuid);
+				if (target == null) {
+					return x_usage(sender);
+				}
+			} else if (args.length == 1) {
+				target = args[0];
+			} else {
+				return x_usage(sender);
+			}
+
+			LAST_X.put(uuid, target);
+
+			if (player.getWorld().getEnvironment() == Environment.NETHER) {
+				block = "ancient_debris";
+			} else {
+				block = "diamond_ore";
+			}
+
+			cmd = String.format("lb lookup destroyed block %s player %s time 0 coords", block, target);
+		} else {
 			return false;
 		}
 
 		Misc.instance.getServer().dispatchCommand(sender, cmd);
 
 		return true;
-
 	}
 
+	private boolean  x_usage(CommandSender sender) {
+		sender.sendMessage(ChatColor.RED + "Incorrect amount of arguments\n" +
+				"Proper syntax is /x <player>");
+		return true;
+	}
 }
