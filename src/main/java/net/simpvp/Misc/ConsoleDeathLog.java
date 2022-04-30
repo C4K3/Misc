1bbba014e9fb7d2b1196df90fe4a945147a2b8e6
Sebastian Blunt <sebastian@c4k3.net>
2022-04-29 20:03:13 -0700

    Switch deathlog to new eventrestore format

--- src/main/java/net/simpvp/Misc/ConsoleDeathLog.java
+++ src/main/java/net/simpvp/Misc/ConsoleDeathLog.java
@@ -1,11 +1,16 @@
 package net.simpvp.Misc;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.util.Base64;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectOutputStream;
 
 /**
  * Logs death messages to console
@@ -30,22 +35,40 @@ public class ConsoleDeathLog implements Listener {
 		/* Log player level and inventory contents just in case.
 		 * The output log format should be consistent with the
 		 * /eventrestore command format */
-		String sInventoryContents = "";
+		ItemStack[] inventory = event.getEntity().getInventory().getContents();
+		String inventoryString;
+		try {
+			ByteArrayOutputStream bytearray = new ByteArrayOutputStream();
+			OutputStream base64 = Base64.getEncoder().wrap(bytearray);
+			BukkitObjectOutputStream bukkitstream = new BukkitObjectOutputStream(base64);
 
-		for (ItemStack itemStack : event.getEntity().getInventory().getContents()) {
-			if (itemStack == null)
-				continue;
+			int len = 0;
+			for (ItemStack itemStack : inventory) {
+				if (itemStack == null) {
+					continue;
+				}
+				len += 1;
+			}
+			bukkitstream.writeInt(len);
 
-			String tmp = itemStack.getType().toString()
-				+ "." + itemStack.getAmount()
-				+ "." + itemStack.getDurability()
-				+ "." + itemStack.getEnchantments().toString();
-			tmp = tmp.replaceAll(" ", "");
-			sInventoryContents += " " + tmp;
+			for (ItemStack itemStack : inventory) {
+				if (itemStack == null) {
+					continue;
+				}
+
+				bukkitstream.writeObject(itemStack);
+			}
+
+			bukkitstream.close();
+			base64.close();
+
+			inventoryString = new String(bytearray.toByteArray());
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
 
 		/* Intentional use of bukkit logger */
-		Bukkit.getLogger().info(death_msg + ": " + event.getEntity().getLevel() + sInventoryContents);
+		Bukkit.getLogger().info(String.format("%s: %d %s", death_msg, event.getEntity().getLevel(), inventoryString));
 	}
 }
 
