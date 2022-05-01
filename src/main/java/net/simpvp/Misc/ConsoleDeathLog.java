1109b132b69f8952a2aca9562374b5d9feb01465
Sebastian Blunt <sebastian@c4k3.net>
2022-05-01 11:25:55 -0700

    Use dedicated InventoryLogger
    To reduce spam

--- src/main/java/net/simpvp/Misc/ConsoleDeathLog.java
+++ src/main/java/net/simpvp/Misc/ConsoleDeathLog.java
@@ -12,6 +12,12 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.io.BukkitObjectOutputStream;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.MarkerManager;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.Level;
+
 /**
  * Logs death messages to console
  */
@@ -20,6 +26,9 @@ public class ConsoleDeathLog implements Listener {
 	public ConsoleDeathLog(Misc plugin) {
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
+
+	private Logger INVENTORY_LOGGER = LogManager.getLogger("InventoryLog");
+	private static final Marker INVENTORY_MARKER = MarkerManager.getMarker("INVENTORY");
 
 	@EventHandler
 	public void onPlayerDeath(PlayerDeathEvent event){
@@ -32,43 +41,45 @@ public class ConsoleDeathLog implements Listener {
 				loc.getBlockZ(),
 				loc.getWorld().getName(),
 				event.getEntity().getName());
+
+		Misc.instance.getLogger().info(death_msg);
+
 		/* Log player level and inventory contents just in case.
 		 * The output log format should be consistent with the
 		 * /eventrestore command format */
-		ItemStack[] inventory = event.getEntity().getInventory().getContents();
-		String inventoryString;
-		try {
-			ByteArrayOutputStream bytearray = new ByteArrayOutputStream();
-			OutputStream base64 = Base64.getEncoder().wrap(bytearray);
-			BukkitObjectOutputStream bukkitstream = new BukkitObjectOutputStream(base64);
+		/* Log it all, just in case */
+		if (INVENTORY_LOGGER.isDebugEnabled()) {
+			try {
+				ItemStack[] inventory = event.getEntity().getInventory().getContents();
+				ByteArrayOutputStream bytearray = new ByteArrayOutputStream();
+				OutputStream base64 = Base64.getEncoder().wrap(bytearray);
+				BukkitObjectOutputStream bukkitstream = new BukkitObjectOutputStream(base64);
 
-			int len = 0;
-			for (ItemStack itemStack : inventory) {
-				if (itemStack == null) {
-					continue;
+				int len = 0;
+				for (ItemStack itemStack : inventory) {
+					if (itemStack == null) {
+						continue;
+					}
+					len += 1;
 				}
-				len += 1;
-			}
-			bukkitstream.writeInt(len);
+				bukkitstream.writeInt(len);
 
-			for (ItemStack itemStack : inventory) {
-				if (itemStack == null) {
-					continue;
+				for (ItemStack itemStack : inventory) {
+					if (itemStack == null) {
+						continue;
+					}
+
+					bukkitstream.writeObject(itemStack);
 				}
 
-				bukkitstream.writeObject(itemStack);
+				bukkitstream.close();
+				base64.close();
+
+				INVENTORY_LOGGER.debug(INVENTORY_MARKER, String.format("%s: %d %s", death_msg, event.getEntity().getLevel(), bytearray.toString()));
+			} catch (Exception e) {
+				Misc.instance.getLogger().warning("Failed to log inventory: " + e);
+				e.printStackTrace();
 			}
-
-			bukkitstream.close();
-			base64.close();
-
-			inventoryString = new String(bytearray.toByteArray());
-		} catch (Exception e) {
-			throw new RuntimeException(e);
 		}
-
-		/* Intentional use of bukkit logger */
-		Bukkit.getLogger().info(String.format("%s: %d %s", death_msg, event.getEntity().getLevel(), inventoryString));
 	}
 }
-
