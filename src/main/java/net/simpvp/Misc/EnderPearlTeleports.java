b66f4d62f56a9bd32a9f8f91c4b4e09d8be8cf72
Laztec <110450926+Laztec@users.noreply.github.com>
2024-08-07 20:37:11 +0100

    Fix cross-dimensional ender pearl teleports

---
+++ src/main/java/net/simpvp/Misc/EnderPearlTeleports.java
@@ -0,0 +1,31 @@
+package net.simpvp.Misc;
+
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class EnderPearlTeleports implements Listener {
+    private final Set<String> allowedWorlds = new HashSet<>(Arrays.asList("world", "world_nether", "world_the_end"));
+
+    @EventHandler
+    public void onPlayerTeleport(PlayerTeleportEvent event) {
+        if (event.getCause() == TeleportCause.ENDER_PEARL) {
+            World fromWorld = event.getFrom().getWorld();
+            World toWorld = event.getTo().getWorld();
+
+            if (!fromWorld.equals(toWorld)) {
+                boolean fromAllowed = allowedWorlds.contains(fromWorld.getName());
+                boolean toAllowed = allowedWorlds.contains(toWorld.getName());
+
+                if (!(fromAllowed && toAllowed)) {
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
+}
